7a7bd4b153c2120879a466270bb89bdee5b8c707
nehal119 <34517705+nehal119@users.noreply.github.com>
2019-11-05 13:50:38 +0530

    email functionality added. Project Issue Tracker introduced.

---
+++ mail.js
@@ -0,0 +1,31 @@
+
+const nodemailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
+
+const auth = {
+    auth: {
+        api_key: process.env.API_KEY ||  "c309b939bf86cb41d2b20e62f67f7dde-f696beb4-635562d3", 
+        domain: process.env.DOMAIN || "sandbox554de596feed477db04b666887e977cd.mailgun.org" 
+    }
+};
+
+const transporter = nodemailer.createTransport(mailGun(auth));
+
+
+const sendMail = (email, subject, text, cb) => {
+    const mailOptions = {
+        from: email,
+        to: "mohammadamiramir773@gmail.com", 
+        subject,
+        text
+    };
+
+    transporter.sendMail(mailOptions, function (err, data) {
+        if (err) {
+            return cb(err, null);
+        }
+        return cb(null, data);
+    });
+}
+
+module.exports = sendMail;
