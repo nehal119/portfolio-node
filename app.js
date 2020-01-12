a72c0382b975d62f7e8a5aa6c58a3b90da8a60d1
Nehal Ahmad <mohammadamiramir773@gmail.com>
2020-01-12 18:31:50 +0530

    compression added

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
 const express = require("express");
+const compression = require("compression");
 const app = express();
+app.use(compression());
 
-app.use(express.static(__dirname + '/public'));
-require('dotenv').config();
-const sendMail = require('./mail');
+app.use(express.static(__dirname + "/public"));
+require("dotenv").config();
+const sendMail = require("./mail");
 
 //Data parsing
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: false
-}));
+  })
+);
 app.use(express.json());
 
-app.post('/email', (req, res) => {
-    const { subject, email, text } = req.body;
-    sendMail(email, subject, text, function(err, data) {
-        if (err) {
-            console.log('ERROR: ', err);
-            return res.status(500).json({ message: err.message || 'Internal Error' });
-        }
-        console.log('Email sent!');
-        return res.json({ message: 'Email sent!' });
-    });
+app.post("/email", (req, res) => {
+  const { subject, email, text } = req.body;
+  sendMail(email, subject, text, function(err, data) {
+    if (err) {
+      console.log("ERROR: ", err);
+      return res.status(500).json({ message: err.message || "Internal Error" });
+    }
+    console.log("Email sent!");
+    return res.json({ message: "Email sent!" });
+  });
 });
-
 
 //Routes Home
 app.get("/", function(req, res) {
-    res.render("index.ejs");
+  res.render("index.ejs");
 });
 
 // Error page
-app.get('/error', (req, res) => {
-    res.render("error.ejs");
+app.get("/error", (req, res) => {
+  res.render("error.ejs");
 });
 
 // Email sent page
-app.get('/email/sent', (req, res) => {
-    res.render("message.ejs");
+app.get("/email/sent", (req, res) => {
+  res.render("message.ejs");
 });
-
 
 //Starting the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
